8ffd311fefef8fc274463b6e4767a40b555992e7
scottyd_14 <scottyd_14@hotmail.com>
2018-10-06 01:29:42 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -102,8 +102,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-    programmer_hash[:known_for] => "GUI"
-    programmer_hash[:known_for]
+     alans_new_info = "GUI"
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
      
      
      
@@ -125,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = ["C","Assembly"]
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
